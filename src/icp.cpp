579b7ec48f6997508bb90e23913c4f1f4ec1b7a5
Dongsheng Yang <ydsf16@163.com>
2019-04-27 19:26:43 +0800

    Update icp.cpp

--- src/icp.cpp
+++ src/icp.cpp
@@ -1,3 +1,5 @@
+// Copyright (C) 2019 Dongsheng Yang <ydsf16@buaa.edu.cn>
+//(Biologically Inspired Mobile Robot Laboratory, Robotics Institute, Beihang University)
 
 #include <icp.h>
 #include <pcl/kdtree/kdtree_flann.h>
