ed0b269879fea7eaa005c7ab68ce25d3f7c07cfa
yds <ydsf16@163.com>
2019-04-30 12:33:12 +0800

    add kmeans

--- include/icp.h
+++ include/icp.h
@@ -1,3 +1,5 @@
+// Copyright (C) 2019 Dongsheng Yang <ydsf16@buaa.edu.cn>
+//(Biologically Inspired Mobile Robot Laboratory, Robotics Institute, Beihang University)
 
 #ifndef ICP_H
 #define ICP_H
--- test/icp_odometry.cpp
+++ test/icp_odometry.cpp
@@ -1,3 +1,6 @@
+// Copyright (C) 2019 Dongsheng Yang <ydsf16@buaa.edu.cn>
+//(Biologically Inspired Mobile Robot Laboratory, Robotics Institute, Beihang University)
+
 #include <iostream>
 #include <algorithm>
 #include <fstream>
